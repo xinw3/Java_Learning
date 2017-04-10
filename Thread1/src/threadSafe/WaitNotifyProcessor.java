0733bd0e9cb96c404abe7e62fe8cdcfe59fd8264
Xin Wang <wangxin94@163.com>
2017-04-10 18:22:21 -0400

    wait and notify

---
+++ Thread1/src/threadSafe/WaitNotifyProcessor.java
@@ -0,0 +1,27 @@
+package threadSafe;
+
+import java.util.Scanner;
+
+public class WaitNotifyProcessor {
+
+	public void produce() throws InterruptedException {
+		synchronized(this) {
+			System.out.println("Producer thread running...");
+			wait();
+			System.out.println("Resumed...");
+		}
+	}
+	public void consume() throws InterruptedException {
+		Scanner scanner = new Scanner(System.in);
+		Thread.sleep(2000);
+		
+		synchronized(this) {
+			System.out.println("Waiting for return key.");
+			scanner.nextLine();
+			System.out.println("Return key pressed.");
+			notify();
+		}
+		
+	}
+
+}
