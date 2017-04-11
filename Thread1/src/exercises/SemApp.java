7a1d2f328c9a35c559889dd28d30de919e3ca4b0
Xin Wang <wangxin94@163.com>
2017-04-11 17:57:46 -0400

    semaphore

---
+++ Thread1/src/exercises/SemApp.java
@@ -0,0 +1,28 @@
+package exercises;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SemApp {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		Connection.getInstance().connect();
+		
+		ExecutorService executor = Executors.newCachedThreadPool();
+		
+		for (int i = 0; i < 200; i++) {
+			executor.submit(new Runnable() {
+				public void run() {
+					Connection.getInstance().connect();
+				}
+			});
+		}
+		
+		executor.shutdown();
+		executor.awaitTermination(1, TimeUnit.DAYS);
+	}
+
+}
