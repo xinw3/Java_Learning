f6e3b3468b41372b5a01d6a78cc77aa9a2ed4a58
Xin Wang <wangxin94@163.com>
2017-04-26 16:14:55 -0400

    fibonacci number

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/eb/f0e588b7bc2a0017154d98721d354fdd
@@ -0,0 +1,10 @@
+package purestorage;
+
+public class Fibbonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Thread1/interview/purestorage/MatrixAverage.java
@@ -0,0 +1,13 @@
+package purestorage;
+
+public class MatrixAverage {
+
+	public void matrixAverage(int[][] matrix) {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
